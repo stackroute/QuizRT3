0aaefbfb29ebbe1a182371c93add354698098cb0
pratsin <pratik.sinha1@wipro.com>
2016-02-08 16:35:18 +0530

    Change in controller

--- public/js/controllers/roundsController.js
+++ public/js/controllers/roundsController.js
@@ -1,25 +1,24 @@
 angular.module("quizRT")
     .controller('roundsController', function($scope, $http, $routeParams, $rootScope, $location){
       $scope.tournamentID=$routeParams.tournamentID;
-      // console.log("Hi, How are you?");
       $rootScope.stylesheetName="topic";
       var path = '/tournamentHandler/tournament/'+$scope.tournamentID;
 
  $http.get(path)
          .success(function(data, status, headers, config) {
-              console.log(data);
+            
               $scope.tournament = data;
-              $rootScope.maxPlayer=data.playersPerMatch;
-              $rootScope.totalMatches=data.matches;
-          }).error(function(data, status, headers, config) {
+              $rootScope.playersPerMatch=data.playersPerMatch;
+        }).error(function(data, status, headers, config) {
              console.log(error);
            });
 
-      $scope.play=function(id)
+      $scope.play=function(levelId,topicId)
       {
-        $rootScope.tournamentID =id;
 
-       var newpath="/quizPlayer";
+        $rootScope.levelId =levelId;
+        $rootScope.tId=topicId;
+         var newpath="/quizPlayer";
         $location.path(newpath);
       };
    });
