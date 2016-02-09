0905738428ebda88cd45c6abbaf2b77ba9ffe2b6
pratsin <pratik.sinha1@wipro.com>
2016-02-09 16:03:00 +0530

    Rounds level no.

--- public/js/controllers/roundsController.js
+++ public/js/controllers/roundsController.js
@@ -19,6 +19,7 @@ angular.module("quizRT")
         $rootScope.title=title;
         $rootScope.tId=topicId;
         $rootScope.topicName=topic_name;
+        $rootScope.roundCount = levelId.substring(str.lastIndexOf("_") + 1);
         $location.path('/quizPlayer');
       };
    });
