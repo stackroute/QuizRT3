8763d82f490f78d389054943ab57c17306903a74
Venkata Maddela <venkata.maddela@wipro.com>
2016-02-12 17:14:10 +0530

    create tournament updated

--- public/js/controllers/createToutnamentController.js
+++ public/js/controllers/createToutnamentController.js
@@ -1,7 +1,15 @@
 angular.module('quizRT')
     .controller('createTournamentController',function($http,$scope,$rootScope,$location,socket){
 
-     
+      $scope.data = 'none';
+     $scope.add = function(){
+       var f = document.getElementById('file').files[0],
+           r = new FileReader();
+       r.onloadend = function(e){
+         $scope.data = e.target.result;
+       }
+       r.readAsBinaryString(f);
+     }
 
 
     });
