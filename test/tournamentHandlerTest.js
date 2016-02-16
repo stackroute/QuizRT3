e051187890ebed86bc81392120021cf2bba0a8c8
Sunil Mekala <sunil.rao15@gmail.com>
2016-02-16 12:40:37 +0530

    added tournamentHanderTest.js

---
+++ test/tournamentHandlerTest.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+
+describe('Test Tournament Handler Router', function (err) {
+
+	it('should test for array size', function () {
+		expect(['1' , '2']).to.have.length(2);
+		//done();
+
+	});
+	
+});
