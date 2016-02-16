bbe2f2fa40d6804c0684c987ec5e33603ab121af
Sunil Mekala <sunil.rao15@gmail.com>
2016-02-16 12:23:57 +0530

    cleared test folder and added few dev dependencies

---
+++ test-data/profileHandler.js
@@ -0,0 +1,39 @@
+//Copyright {2016} {NIIT Limited, Wipro Limited}
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//   
+//   Name of Developers  Raghav Goel, Kshitij Jain, Lakshay Bansal, Ayush Jain, Saurabh Gupta, Akshay Meher
+//  
+ 
+var express = require('express');
+var fs = require('fs');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Profile;
+var profileData;
+
+var Profile = require("../models/profile");
+// var Topic=require("../models/Topic");
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://172.23.238.253/quizRT');
+
+var db = mongoose.connection;
+  // console.log("this is form profile data"+req.params.id);
+    Profile.findOne({userId: "LA1"})
+      .populate("topicsPlayed.topicId")
+          .exec(function(err,data){
+            profileData = data;
+            console.log(JSON.stringify(profileData, null, 4));
+
+          });
