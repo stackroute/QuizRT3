0f6fd64b38b61ccbff5c51187e360bd3f7953aeb
Anil Sawant <anilsawant241191@gmail.com>
2016-02-25 13:42:38 +0530

    removed log

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ var initPassport = require('./passport-init');
 initPassport(passport);
 
 
-middleware to check if user session exists, and check for isAuthenticated cookie
+//middleware to check if user session exists, and check for isAuthenticated cookie
 app.use( function( req, res, next ) {
   if( req.cookies.isAuthenticated || (req.session && req.session.user) ) {
     next();
@@ -101,7 +101,6 @@ app.use( function( req, res, next ) {
 //     res.end( JSON.stringify( { error: 'User session does not exist. Kindly do a fresh login.'} ));
 //   }
 // });
->>>>>>> 82b558b3ec4acda51b2e9514157cebc6b43241ae
 app.use('/userProfile', profileHandler);
 app.use('/topicsHandler', topicsHandler);
 app.use('/tournamentHandler', tournamentHandler);
