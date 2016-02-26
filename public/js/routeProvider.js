77d8ffcaaad6e5a27bedd5bc1c92eccfd90d4f33
Anil Sawant <anilsawant241191@gmail.com>
2016-02-26 19:46:37 +0530

    changed socket connect url to quizrt.stackroute.in. Very imp to make this change.

--- public/js/routeProvider.js
+++ public/js/routeProvider.js
@@ -102,7 +102,7 @@ angular.module('quizRT', ['ngRoute', 'ngCookies'])
 
       return function($rootScope, type) {
         // var socket = io.connect('http://172.23.238.188:8080' + type, {'forceNew':true } );
-        var socket = io.connect('http://52.89.104.235:2000' + type, {'forceNew':true } );
+        var socket = io.connect('http://quizart.stackroute.in:2000' + type, {'forceNew':true } );
         console.log('Socket initialized for ' + type);
 
         return {
